b0ceb370bf5549869f75382b31ee32f1e50d6329
Ali Bingul <alibingul40@gmail.com>
2023-04-21 23:58:02 +0300

    project controller created

---
+++ controllers/projectController.js
@@ -0,0 +1,75 @@
+const projectService = require("../services/projectService");
+const { generateMesage } = require("../util/messageGenerator");
+
+const getAllProjects = (req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    projectService.getAllProjects(
+        req.query.where,
+        req.query.limit,
+        req.query.offset,
+        req.query.year,
+        req.query.term
+    ).then((projectDatas) => {
+        res.statusCode = 200;
+        res.json(generateMesage(true, projectDatas));
+    }).catch((err) => {
+        res.statusCode = 400;
+        res.json(generateMesage(false, null, err.message));
+    });
+};
+
+const getSingleProject = (req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    projectService.getSingleProject(req.params.projectId)
+        .then((projectData) => {
+            res.statusCode = 200;
+            res.json(generateMesage(true, projectData));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMesage(false, null, err.message));
+        });
+};
+
+const insertProject = (req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    projectService.insertProject(req.body)
+        .then((insertProjectResponse) => {
+            res.statusCode = 200;
+            res.json(generateMesage(true, insertProjectResponse));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMesage(false, null, err.message));
+        });
+};
+
+const updateProject = (req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    projectService.updateProject(req.body, req.params.projectId)
+        .then((updatedProjectsCount) => {
+            res.statusCode = 200;
+            res.json(generateMesage(true, updatedProjectsCount));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMesage(false, null, err.message));
+        });
+};
+
+const deleteProject = (req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    projectService.deleteProject(req.params.projectId)
+        .then((deletedProjectsCount) => {
+            res.statusCode = 200;
+            res.json(generateMesage(true, deletedProjectsCount));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMesage(false, null, err.message));
+        });
+};
+
+module.exports = {
+    getAllProjects,
+    getSingleProject,
+    insertProject,
+    updateProject,
+    deleteProject
+};
