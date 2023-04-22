3d3182cead2559df850ac495ec692724d7b71ff4
Ali Bingul <alibingul40@gmail.com>
2023-04-22 18:53:23 +0300

    project document controller finished

--- controllers/projectDocumentController.js
+++ controllers/projectDocumentController.js
@@ -26,10 +26,32 @@ const insertProjectDocument = (req, res, next) => {
 };
 
 const updateProjectDocumentTitle = (req, res, next) => {
-    
+    res.setHeaders("Content-Type", "application/json");
+    projectDocumentService.updateProjectDocumentTitle(req.body.title, req.params.projectDocumentId)
+        .then((updatedProjectDocumentsCount) => {
+            res.statusCode = 200;
+            res.json(generateMessage(true, updatedProjectDocumentsCount));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMessage(false, null, err.message));
+        });
+};
+
+const deleteSingleProjectDocument = (req, res, next) => {
+    res.setHeaders("Content-Type", "application/json");
+    projectDocumentService.deleteSingleProjectDocument(req.params.projectDocumentId)
+        .then((deletedProjectDocumentsCount) => {
+            res.statusCode = 200;
+            res.json(generateMessage(true, deletedProjectDocumentsCount));
+        }).catch((err) => {
+            res.statusCode = 400;
+            res.json(generateMessage(false, null, err.message));
+        });
 };
 
 module.exports = {
     getProjectDocuments,
     insertProjectDocument,
+    updateProjectDocumentTitle,
+    deleteSingleProjectDocument
 };
